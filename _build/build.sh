8083fbcfb16e0982a0b7690954fd9f6a06ef0805
zeruhur <51272598+zeruhur@users.noreply.github.com>
2022-03-07 14:14:10 +0100

    Update build.sh

--- _build/build.sh
+++ _build/build.sh
@@ -1,1 +1,1 @@
-pandoc -f gfm -t html -o pdf/animaprime.pdf -s md/animaprime.md --pdf-engine=weasyprint --css="css/animaprime.css" --metadata pagetitle="Anima Prime" --columns=2
+pandoc -f gfm -t html -o pdf/animaprime.pdf -s md/animaprime.md --pdf-engine=weasyprint --css="css/animaprime.css" -V lang=it
